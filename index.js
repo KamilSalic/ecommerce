a043004776253114211c7252209ebef16c13fa4d
KamilSalic <kamilsalic@gmail.com>
2022-01-03 20:55:24 +0000

    added tablet media queries and changed header

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
-const menuOpen= document.getElementById("mmenu");
-const hamburger= document.getElementById("burger");
-const menuClose= document.getElementById("closse");
+const menuOpen = document.getElementById("menu");
+const hamburger = document.getElementById("burger");
+const menuClose = document.getElementById("close");
 
-function openMenu(){
+function openMenu() {
     menuOpen.classList.add("menuopacity");
 }
-function closeMenu(){
+function closeMenu() {
     menuOpen.classList.remove("menuopacity");
 }
 
 hamburger.addEventListener('click', openMenu);
 menuClose.addEventListener('click', closeMenu);
+
+const prevArrow = document.getElementsByClassName('prev');
+const circleImgSlider = documennt.getElementsByClassName('circle-img-slider');
+
+prevArrow.addEventListener('click', function () {
+    circleImgSlider.style.overflowX = "30px";
+})
