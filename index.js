ccd1c4c5a4b59088b573e26ad5ad4f125cb3c820
KamilSalic <kamilsalic@gmail.com>
2022-01-07 01:10:03 +0000

    refactored css and html class names, added carousel effect

--- index.js
+++ index.js
@@ -12,9 +12,58 @@ function closeMenu() {
 hamburger.addEventListener('click', openMenu);
 menuClose.addEventListener('click', closeMenu);
 
-const prevArrow = document.getElementsByClassName('prev');
-const circleImgSlider = documennt.getElementsByClassName('circle-img-slider');
 
-prevArrow.addEventListener('click', function () {
-    circleImgSlider.style.overflowX = "30px";
-})
+const prevArrow = document.getElementById('prev-chevron');
+const nextArrow = document.getElementById('next-chevron');
+const sliderInner = document.getElementById('circle-slider-inner');
+
+const specialOfferInner = document.getElementById('special-offer-inner');
+const prevArrowSpecial = document.getElementById('prev-chevron-special-offer');
+const nextArrowSpecial = document.getElementById('next-chevron-special-offer');
+
+const newArrivalsInner = document.getElementById('new-arrivals-inner');
+const prevArrowArrivals = document.getElementById('prev-chevron-new-arrivals');
+const nextArrowArrivals = document.getElementById('next-chevron-new-arrivals');
+
+function moveRight() {
+    sliderInner.classList.remove("transLeft");
+    sliderInner.classList.add("transRight");
+}
+function moveLeft() {
+    sliderInner.classList.remove("transRight");
+    sliderInner.classList.add("transLeft");
+}
+
+function moveRightSpecial() {
+    specialOfferInner.classList.remove("transLeft");
+    specialOfferInner.classList.add("transRight");
+    specialOfferInner.style.transform = "translate3d(-50%, 0, 0)";
+}
+function moveLeftSpecial() {
+    specialOfferInner.classList.remove("transRight");
+    specialOfferInner.style.transform = "translate3d(0px, 0, 0)";
+    specialOfferInner.classList.add("transLeft");
+}
+
+function moveRightArrivals() {
+    newArrivalsInner.classList.remove("transLeft");
+    newArrivalsInner.classList.add("transRight");
+    newArrivalsInner.style.transform = "translate3d(-50%, 0, 0)";
+    // if (newArrivalsInner.classList.contains('transRight')) {
+    //     newArrivalsInner.style.overflow = "hidden";
+    // }
+
+}
+function moveLeftArrivals() {
+    newArrivalsInner.classList.remove("transRight");
+    newArrivalsInner.style.transform = "translate3d(0px, 0, 0)";
+    newArrivalsInner.classList.add("transLeft");
+}
+nextArrow.addEventListener('click', moveRight);
+prevArrow.addEventListener('click', moveLeft);
+
+nextArrowSpecial.addEventListener('click', moveRightSpecial);
+prevArrowSpecial.addEventListener('click', moveLeftSpecial);
+
+nextArrowArrivals.addEventListener('click', moveRightArrivals);
+prevArrowArrivals.addEventListener('click', moveLeftArrivals);
